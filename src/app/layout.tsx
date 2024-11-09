d2acb79da3d73c3b23c3a9852bec85305ae625a5
Mohammad Faizan <faizancurious@gmail.com>
2024-11-09 19:59:54 +0530

    minor meta tag fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,23 +65,6 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <head>
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon/favicon-96x96.png"
-          sizes="96x96"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <meta name="apple-mobile-web-app-title" content="Pouzz" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-      </head>
       <body>
         {children}
         <Toaster />
